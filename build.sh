a11be947c36c9f3580660bd48e887093b2cbd924
ALee <limenleaf@gmail.com>
2023-09-16 16:41:06 -0500

    e12

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ source venv/bin/activate
 
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
-python3.9 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
