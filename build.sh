ad21ee994b3a17affe90d73858a463cb217b8c06
Eddie Tribaldos <EdDT.phi@gmail.com>
2023-09-16 15:19:11 -0700

    Update build.sh
    Back to 3.9

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ source venv/bin/activate
 
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Make migrations
 # echo "Making migrations..."
@@ -20,4 +20,4 @@ python -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
